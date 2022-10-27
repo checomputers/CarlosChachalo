c43799c67521de175077d45199704628c85de55a
Carlos Chachalo <checomputers@gmail.com>
2022-10-26 22:48:50 -0500

    Agregar archivos de proyecto.

---
+++ CarlosChachalo/Login.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CarlosChachalo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnIngresar_Clicked(object sender, EventArgs e)
+        {
+         string usu = "carlos2022";
+            string contra = "uisrael2022";
+
+            string tusu = txtUsuario.Text;
+            string tcontra = txtContrasena.Text;
+
+            if (usu == tusu & contra == tcontra)
+            {
+
+                await DisplayAlert("BIENVENIDO", "USUARIO CORRECTO", "ACEPTAR");
+                await Navigation.PushAsync(new Registro(txtUsuario.Text));
+            }
+
+            else
+            {
+                await DisplayAlert("ERROR", "USUARIO O CONTRASEÑA INCORRECTA", "CERRAR");
+            }
+        }
+    }
+}
---
+++ CarlosChachalo/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace CarlosChachalo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
